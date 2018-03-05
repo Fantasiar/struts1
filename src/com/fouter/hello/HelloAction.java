40c9826b946f0598ab4d8eb4d5fa99cfe3aed95d
Fouter <1420126933@qq.com>
2018-03-05 14:31:17 +0800

    the first struts

---
+++ src/com/fouter/hello/HelloAction.java
@@ -0,0 +1,11 @@
+package com.fouter.hello;
+
+import com.sun.org.apache.regexp.internal.recompile;
+
+public class HelloAction {
+	public String hello() {
+		System.out.println("helllo world!");
+		
+		return "success";
+	}
+}
