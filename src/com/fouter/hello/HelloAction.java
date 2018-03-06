27d55d504c2ee58cac8861984413ee4746f2a028
Fouter <1420126933@qq.com>
2018-03-06 19:52:30 +0800

    the second training of struts

--- src/com/fouter/hello/HelloAction.java
+++ src/com/fouter/hello/HelloAction.java
@@ -1,11 +1,24 @@
 package com.fouter.hello;
 
+import org.omg.CORBA.UnknownUserException;
+
+import com.fouter.domain.User;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
 import com.sun.org.apache.regexp.internal.recompile;
 
-public class HelloAction {
+public class HelloAction extends ActionSupport implements ModelDriven<User> {
+	private User user=new User();
+	
 	public String hello() {
-		System.out.println("helllo world!");
+		System.out.println(user);
 		
 		return "success";
+	}
+
+	@Override
+	public User getModel() {
+		// TODO Auto-generated method stub
+		return user;
 	}
 }
